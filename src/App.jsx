18582b2d8ddedca4fb34660433050c9f63d0f845
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-11-03 22:37:41 -0400

    starting ecomerce ,card material-ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Product from "./components/Product";
+
+function App() {
+  return (
+    <>
+      <div>App</div>
+      <Product />
+    </>
+  );
+}
+
+export default App;
